a2e08f14d057aca406e7b9de8c109d980c1e1a99
root <root@ip-10-0-1-174.us-west-1.compute.internal>
2023-07-06 23:03:59 +0000

    Second[Agent] cra to next.js

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../App.css'
+import '../index.css'
+import Head from 'next/head'
+
+export default function MyApp({ Component, pageProps}) {
+  return (
+    <>
+      <Head>
+        <title>React App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  )
+}
